1e51e9b3b008018a83433e9d3870b50c66c7d96d
Andree Valle Campos <avallecam@gmail.com>
2025-05-27 12:59:02 +0100

    add installation and register in renv lock file

---
+++ 00-installation.R
@@ -0,0 +1,69 @@
+if(!require("pak")) install.packages("pak")
+
+new_packages <- c(
+  # for Introduction tutorial
+  "here",
+  "tidyverse",
+  "visdat",
+  "skimr",
+  "rmarkdown",
+  "quarto",
+  # for Early Task tutorials
+  "epiverse-trace/cleanepi",
+  "rio",
+  "DBI",
+  "RSQLite",
+  "dbplyr",
+  # "linelist",
+  "epiverse-trace/simulist",
+  "incidence2",
+  "epiverse-trace/tracetheme"#,
+  # for Middle Task tutorials
+  # "EpiNow2",
+  # "epiparameter",
+  # "cfr",
+  # "outbreaks",
+  # "epicontacts",
+  # "fitdistrplus",
+  # "superspreading",
+  # "epichains",
+  # # for Late task tutorials
+  # "socialmixr",
+  # "finalsize",
+  # "epiverse-trace/epidemics",
+  # "scales"
+)
+
+pak::pak(new_packages)
+
+# for Introduction tutorial
+library(here)
+library(tidyverse)
+library(visdat)
+library(skimr)
+library(rmarkdown)
+library(quarto)
+# for Early Task tutorials
+library(cleanepi)
+library(rio)
+library(DBI)
+library(RSQLite)
+library(dbplyr)
+# library(linelist)
+library(simulist)
+library(incidence2)
+library(tracetheme)
+# # for Middle Task tutorials
+# library(EpiNow2)
+# library(epiparameter)
+# library(cfr)
+# library(outbreaks)
+# library(epicontacts)
+# library(fitdistrplus)
+# library(superspreading)
+# library(epichains)
+# # for Late task tutorials
+# library(socialmixr)
+# library(finalsize)
+# library(epidemics)
+# library(scales)
